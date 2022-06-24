8d95ed059bfae4ff15ad3e09bffba86ee9c0f983
Sasha Cocquyt <106103073+sCocquyt@users.noreply.github.com>
2022-06-24 10:40:13 -0700

    Comments for Arduino factorial program

--- Arduino_Factorial/Arduino_Factorial.ino
+++ Arduino_Factorial/Arduino_Factorial.ino
@@ -1,3 +1,4 @@
+// initialize variables
 int factorial;
 int counter;
 double total = 1;
@@ -10,13 +11,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  // obtain the number to factorialize from the arduino
   factorial = Serial.read()-'0';
+  
+  // set the counter equal to the numer to factorialize (dictates how many multiplication operations will occur)
   counter = factorial;
+  
+  // carry out the factorialization using a while loop until counter is equal to 1
   if (factorial > 0) {
     while (counter > 1) {
       total *= counter;
       counter--;
     }
+    
+    // shows the factorialization of the input number on the arduino
     Serial.print(factorial);
     Serial.print("! = ");
     Serial.println(total);
